bbd14f8c1b50ad558131b247cca3f338d1b12771
Adam Stone <astone.stone@gmail.com>
2020-02-06 23:17:22 -0800

    initial editing of comments

--- download_prov_csvs.py
+++ download_prov_csvs.py
@@ -6,6 +6,6 @@ prov_base = 'https://data.cms.gov/api/views/ykfi-ffzq/rows.csv?accessType=DOWNLO
 #This file is a point in time snapshot of select practice location information for active enrollments
 prov_addy = 'https://data.cms.gov/api/views/je57-c47h/rows.csv?accessType=DOWNLOAD&bom=true&format=true'
 
-#get files and write to project folder/Users/aston/PyScript/cms_prov_data/
+#get the files and write to project folder/Users/aston/PyScript/cms_prov_data/
 urllib.request.urlretrieve(prov_base, '/Users/aston/PyScript/cms_prov_data/Base_Provider_Enrollment_File.csv')
 urllib.request.urlretrieve(prov_addy, '/Users/aston/PyScript/cms_prov_data/Address_Provider_Enrollment_File.csv')
